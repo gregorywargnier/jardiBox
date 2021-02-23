0b59dcf3e35d8c214439ce7e808990ae75ab60dd
greg <greg@drdata.io>
2021-02-23 16:08:03 +0100

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+plugins: {
+    // include whatever plugins you want
+    // but make sure you install these via yarn or npm!
+
+    // add browserslist config to package.json (see below)
+    autoprefixer: {}
+}
