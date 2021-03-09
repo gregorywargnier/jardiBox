e52ce6b30fe5c6771ac46b910011a30b3a0f0b73
greg <greg@drdata.io>
2021-03-09 23:28:10 +0100

    create database contact

---
+++ src/Form/ContactType.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('lastName', TextType::class, [
+                "label" => "Nom",
+                "attr" => [
+                    'class' => "form-control",
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Saisir votre nom",
+                    ])
+                ]
+                ])
+            ->add('firstName' , TextType::class, [
+                "label" => "Prénom",
+                "attr" => [
+                    'class' => "form-control",
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Saisir votre prénom",
+                    ])
+                ]
+            ])
+            ->add('mail',EmailType::class, [
+                "label" => "Email",
+                "attr" => [
+                    "class" => "form-control",
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Saisir votre email",
+                    ])
+                ]
+            ])
+            ->add('description',TextareaType::class, [
+        "label" => "description",
+        "attr" => [
+            'class' => "form-control",
+        ],
+        'constraints' => [
+            new NotBlank([
+                'message' => "Veuillez décrire votre problématique",
+            ])
+        ]
+    ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
