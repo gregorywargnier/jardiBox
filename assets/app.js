0679004a0092e80a891565d67ac3b99d9cb93c1e
greg <greg@drdata.io>
2021-03-10 02:36:16 +0100

    fix

--- assets/app.js
+++ assets/app.js
@@ -19,3 +19,8 @@ require('@fortawesome/fontawesome-free/css/all.min.css');
 require('@fortawesome/fontawesome-free/js/all.js');
 
 
+
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
+
+
