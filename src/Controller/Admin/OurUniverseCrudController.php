0dee888ab4bfef58394d370d6ea2be481eb10f02
greg <greg@drdata.io>
2021-04-16 11:14:51 +0200

    bdd refont

--- src/Controller/Admin/OurUniverseCrudController.php
+++ src/Controller/Admin/OurUniverseCrudController.php
@@ -4,13 +4,24 @@ namespace App\Controller\Admin;
 
 use App\Entity\OurUniverse;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class OurUniverseCrudController extends AbstractCrudController
 {
+    private $params;
+
+    public function __construct(ParameterBagInterface $params)
+    {
+        $this->params = $params;
+    }
     public static function getEntityFqcn(): string
     {
         return OurUniverse::class;
@@ -23,7 +34,15 @@ class OurUniverseCrudController extends AbstractCrudController
             IdField::new('id')->hideOnForm(),
             TextField::new('title'),
             TextEditorField::new('description'),
+            ImageField::new('fileName')
+                ->onlyOnIndex()
+                ->setBasePath('dataImage/products'),
             TextField::new('slug'),
+            TextareaField::new('imageFile')
+                ->setFormType(VichImageType::class)
+                ->setFormTypeOption('allow_delete', false)
+                ->hideOnIndex()
+
         ];
     }
 
