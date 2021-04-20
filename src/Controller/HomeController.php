984a955bcb7ac0e7e58b798018f426339fbc8489
greg <greg@drdata.io>
2021-04-20 13:52:35 +0200

    allCategories finish

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,15 +4,18 @@ namespace App\Controller;
 
 use App\Repository\UniverseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends AbstractController
 {
-    public function __invoke(UniverseRepository $universeRepo): Response
+    public function __invoke(UniverseRepository $universeRepo, Request $request): Response
     {
         $universe = $universeRepo->findAll();
+
         return $this->render('home/index.html.twig', [
             'universe' => $universe,
+
         ]);
     }
 }
